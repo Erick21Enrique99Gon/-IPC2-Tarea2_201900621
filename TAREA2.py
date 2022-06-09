b134b60bedfbd3c4c1f5f42db19709db598917c7
Erick21Enrique99Gon <61717279+Erick21Enrique99Gon@users.noreply.github.com>
2022-06-08 19:31:04 -0600

    final

--- TAREA2.py
+++ TAREA2.py
@@ -9,7 +9,7 @@ class lista:
         self.inicio = None
         self.final = None
     
-    def encolar(self,dato):
+    def push(self,dato):
         nuevoNodo = Nodo(dato)
 
         if  self.inicio == None:
@@ -19,18 +19,47 @@ class lista:
             nuevoNodo.anterior = self.final
             self.final.siguiente = nuevoNodo
             self.final = nuevoNodo
+            self.final.siguiente = self.inicio
+            self.inicio.anterior = self.final
     
-    def desencolar(self):
+    def pop(self):
         if self.inicio == None:
             return
         else:
             nodoaux = self.inicio
             self.inicio = self.inicio.siguiente
-            self.inicio.anterior = None
+            self.inicio.anterior = self.final
+            self.final.siguiente = self.inicio
             return nodoaux.dato
     
     def imprimir(self):
         aux = self.inicio
         while aux != None:
             print(aux.dato)
+            if aux.siguiente == self.inicio:
+                return
             aux = aux.siguiente
+
+    def datos_numero(self,numero):
+        aux = self.inicio
+        print('Numero:',numero)    
+        while aux != None:
+            if aux.dato == numero:
+                print('Numero anterior:',aux.anterior.dato)
+                print('Numero porterior:',aux.siguiente.dato)
+                return
+            if aux.siguiente == self.inicio:
+                return
+            aux = aux.siguiente
+
+
+l = lista()
+l.push(1)
+l.push(2)
+l.push(3)
+l.push(4)
+l.push(5)
+print('----------')
+l.imprimir()
+print('----------')
+l.datos_numero(5)
